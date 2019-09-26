78c0bb5fbc8050fca2f84111c42165f7ad462e74
Sam Kalish <samk825@gmail.com>
2019-09-26 10:10:33 -0400

    Initial commit.

---
+++ hopscotch/Rec_Model.py
@@ -0,0 +1,82 @@
+import numpy as np
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from scipy.sparse import hstack
+from nltk.tokenize import RegexpTokenizer
+
+tokenizer = RegexpTokenizer(r'\w+')
+
+# Count vectorizer function
+def cv(data):
+    count_vectorizer = TfidfVectorizer()
+
+    emb = count_vectorizer.fit_transform(data)
+
+    return emb, count_vectorizer
+
+# Generate cosine similarity matrix
+def gen_cos_sim(df):
+    # Vectorize nose notes
+    counts, counts_vectorizer = cv(df["Nose"].tolist())
+
+    # Vectorize palate notes and catenate to current array
+    counts2, counts_vectorizer = cv(df["Palate"].tolist())
+    counts = hstack((counts, counts2))
+
+    # Vectorize finish notes and catenate to current array
+    counts2, counts_vectorizer = cv(df["Finish"].tolist())
+    counts = hstack((counts, counts2))
+
+    # Return cosine similarity matrix
+    return cosine_similarity(counts)
+
+def ApplyModel(rec_df, liked_list = [], disliked_list = []):
+    # Generate tokenized note descriptions
+    rec_df['Nose_tokens'] = rec_df['Nose_clean'].apply(tokenizer.tokenize)
+    rec_df['Palate_tokens'] = rec_df['Palate_clean'].apply(tokenizer.tokenize)
+    rec_df['Finish_tokens'] = rec_df['Finish_clean'].apply(tokenizer.tokenize)
+
+
+    # Initialize lists of liked and disliked notes
+    nose_liked_notes = []
+    nose_disliked_notes = []
+    palate_liked_notes = []
+    palate_disliked_notes = []
+    finish_liked_notes = []
+    finish_disliked_notes = []
+    
+    # Set user_pref column values from lists supplied by user
+    rec_df['user_pref'] = np.zeros(rec_df.shape[0])
+    for name in liked_list:
+        rec_df.loc[rec_df['name'] == name, 'user_pref'] = 1
+        nose_liked_notes.extend(rec_df.loc[rec_df['name'] == name, 'Nose_tokens'].values[0])
+        palate_liked_notes.extend(rec_df.loc[rec_df['name'] == name, 'Palate_tokens'].values[0])
+        finish_liked_notes.extend(rec_df.loc[rec_df['name'] == name, 'Finish_tokens'].values[0])
+    for name in disliked_list:
+        rec_df.loc[rec_df['name'] == name, 'user_pref'] = -1
+        nose_disliked_notes.extend(rec_df.loc[rec_df['name'] == name, 'Nose_tokens'].values[0])
+        palate_disliked_notes.extend(rec_df.loc[rec_df['name'] == name, 'Palate_tokens'].values[0])
+        finish_disliked_notes.extend(rec_df.loc[rec_df['name'] == name, 'Finish_tokens'].values[0])
+
+    nose_liked_notes = set(nose_liked_notes) - set(nose_disliked_notes)
+    
+    palate_liked_notes = set(palate_liked_notes) - set(palate_disliked_notes)
+    
+    finish_liked_notes = set(finish_liked_notes) - set(finish_disliked_notes)
+    
+    
+    # Generate cosine similarity matrix
+    cos_sim = gen_cos_sim(rec_df)
+    
+    # Calculate scores and store in rec_df
+    rec_df['score'] = list(np.array(rec_df['user_pref']).dot(cos_sim))
+    #rec_df['Nose_common'] = rec_df['Nose_tokens'].apply(type)
+    rec_df['Nose_common'] = rec_df['Nose_tokens'].apply(set).apply(nose_liked_notes.intersection).apply(list).apply(', '.join)
+    rec_df['Palate_common'] = rec_df['Palate_tokens'].apply(set).apply(nose_liked_notes.intersection).apply(list).apply(', '.join)
+    rec_df['Finish_common'] = rec_df['Finish_tokens'].apply(set).apply(nose_liked_notes.intersection).apply(list).apply(', '.join)
+    
+    # Sort by score and exclude items that have already been rated
+    result = rec_df[rec_df['user_pref'] == 0].sort_values(by='score', ascending = False)
+    
+    return result
---
+++ hopscotch/views.py
@@ -0,0 +1,113 @@
+from flask import render_template
+from flask import request
+from hopscotch import app
+from hopscotch.Rec_Model import ApplyModel
+from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database
+import pandas as pd
+import psycopg2
+
+# Python code to connect to Postgres
+user = 'skalish'
+host = 'localhost'
+dbname = 'scotch_db'
+db = create_engine('postgres://%s%s/%s'%(user,host,dbname))
+con = None
+con = psycopg2.connect(database = dbname, user = user, host = host, password = 'password')
+
+@app.route('/index')
+def index():
+    return render_template("index.html",
+       title = 'Home', user = { 'nickname': 'Visitor' }
+       )
+
+@app.route('/db')
+def scotch_page():
+    sql_query = """
+                SELECT * FROM scotch_data_table_clean;
+                """
+    query_results = pd.read_sql_query(sql_query,con)
+    scotches = ""
+    for i in range(0,100):
+        scotches += query_results.iloc[i]['Region']
+        scotches += "<br>"
+    return scotches
+
+@app.route('/db_fancy')
+def scotch_page_fancy():
+    sql_query = """
+                SELECT * FROM scotch_data_table_clean;
+                """
+    query_results=pd.read_sql_query(sql_query,con)
+    query_results=query_results.sort_values(by='name')
+    scotches = []
+    for i in range(0,query_results.shape[0]):
+        scotches.append(dict(name=query_results.iloc[i]['name'],
+                           region=query_results.iloc[i]['Region']))
+    return render_template('scotches.html', scotches=scotches)
+
+@app.route('/')
+@app.route('/input')
+def scotch_input():
+    # get list of scotches
+    query = "SELECT name FROM scotch_data_table_clean"
+    print(query)
+    query_results = pd.read_sql_query(query,con)
+    print(query_results)
+    scotch_list = []
+    for i in range(0,query_results.shape[0]):
+        scotch_list.append(query_results.iloc[i]['name'])
+
+    return render_template("input.html",
+                           scotch_list = scotch_list)
+
+@app.route('/output')
+def scotch_output():
+    query = "SELECT name FROM scotch_data_table_clean"
+    print(query)
+    query_results = pd.read_sql_query(query,con)
+    print(query_results)
+    scotch_list = []
+    for i in range(0,query_results.shape[0]):
+        scotch_list.append(query_results.iloc[i]['name'])
+
+    
+    # Pull 'liked' and 'disliked' scotch names from input fields and store them
+    good_products = request.args.getlist('likes')
+    bad_products = request.args.getlist('dislikes')
+
+    # Pull price range from input fields
+    low_price = request.args.get('low_price')
+    high_price = request.args.get('high_price')
+    print(low_price)
+    print(high_price)
+    
+    # Fill dataframe from scotch database
+    rec_query = "SELECT * FROM scotch_data_table_clean"
+    rec_df=pd.read_sql_query(rec_query,con)
+
+    # Apply model and get dataframe sorted by score
+    scored_df = ApplyModel(rec_df, good_products, bad_products)
+
+    # Filter by user price range
+    scored_df = scored_df[scored_df.price_usd >= int(low_price)]
+    scored_df = scored_df[scored_df.price_usd <= int(high_price)]
+    
+    # Recommend scotches only from 800 most popular products (optional)
+    scored_df = scored_df[scored_df.index < 800]
+
+    scotches = []
+    for i in range(10):
+        print(scored_df.iloc[i]['Nose_common'])
+        scotches.append(dict(index=scored_df.iloc[i]['index'],
+                             name=scored_df.iloc[i]['name'],
+                             price=scored_df.iloc[i]['price_usd'],
+                             region=scored_df.iloc[i]['Region'],
+                             score=scored_df.iloc[i]['score'],
+                             nose=scored_df.iloc[i]['Nose_common'],
+                             palate=scored_df.iloc[i]['Palate_common'],
+                             finish=scored_df.iloc[i]['Finish_common']))
+    
+    return render_template("output.html",
+                           scotch_list = scotch_list,
+                           scotches = scotches)
